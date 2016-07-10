95310a2d6d2fa5011738fca58952d7b792cf1de4
Logan Gorence <loganjohngorence@gmail.com>
2016-07-10 13:08:26 -0700

    Clean up the DOOM main file.

--- doomclassic/doom/d_items.cpp
+++ doomclassic/doom/d_items.cpp
@@ -26,8 +26,6 @@ If you have questions concerning this license or the applicable additional terms
 ===========================================================================
 */
 
-#include "Precompiled.h"
-
 #ifdef __GNUG__
 #pragma implementation "d_items.h"
 #endif
@@ -35,112 +33,98 @@ If you have questions concerning this license or the applicable additional terms
 
 #include "globaldata.h"
 
-// We are referring to sprite numbers.
-#include "info.h"
-
-
-//
-// PSPRITE ACTIONS for waepons.
-// This struct controls the weapon animations.
-//
-// Each entry is:
-//   ammo/amunition type
-//  upstate
-//  downstate
-// readystate
-// atkstate, i.e. attack/fire/hit frame
-// flashstate, muzzle flash
-//
+/**
+ * PSPRITE ACTIONS for weapons.
+ * This struct controls the weapon animations.
+ * Each entry is made up of:
+ * - Ammo/ammunition type
+ * - Up state
+ * - Down state
+ * - Ready state
+ * - Attack/fire/hit frame
+ * - Muzzle flash state
+ */
 const weaponinfo_t	weaponinfo[NUMWEAPONS] =
 {
     {
-	// fist
-	am_noammo,
-	S_PUNCHUP,
-	S_PUNCHDOWN,
-	S_PUNCH,
-	S_PUNCH1,
-	S_NULL
-    },	
-    {
-	// pistol
-	am_clip,
-	S_PISTOLUP,
-	S_PISTOLDOWN,
-	S_PISTOL,
-	S_PISTOL1,
-	S_PISTOLFLASH
-    },	
-    {
-	// shotgun
-	am_shell,
-	S_SGUNUP,
-	S_SGUNDOWN,
-	S_SGUN,
-	S_SGUN1,
-	S_SGUNFLASH1
+        // Fists
+        am_noammo,
+        S_PUNCHUP,
+        S_PUNCHDOWN,
+        S_PUNCH,
+        S_PUNCH1,
+        S_NULL
     },
     {
-	// chaingun
-	am_clip,
-	S_CHAINUP,
-	S_CHAINDOWN,
-	S_CHAIN,
-	S_CHAIN1,
-	S_CHAINFLASH1
+        // Pistol
+        am_clip,
+        S_PISTOLUP,
+        S_PISTOLDOWN,
+        S_PISTOL,
+        S_PISTOL1,
+        S_PISTOLFLASH
+    },
+	{
+        // Shotgun
+        am_shell,
+        S_SGUNUP,
+        S_SGUNDOWN,
+        S_SGUN,
+        S_SGUN1,
+        S_SGUNFLASH1
     },
     {
-	// missile launcher
-	am_misl,
-	S_MISSILEUP,
-	S_MISSILEDOWN,
-	S_MISSILE,
-	S_MISSILE1,
-	S_MISSILEFLASH1
+        // Chain Gun
+        am_clip,
+        S_CHAINUP,
+        S_CHAINDOWN,
+        S_CHAIN,
+        S_CHAIN1,
+        S_CHAINFLASH1
     },
     {
-	// plasma rifle
-	am_cell,
-	S_PLASMAUP,
-	S_PLASMADOWN,
-	S_PLASMA,
-	S_PLASMA1,
-	S_PLASMAFLASH1
+        // Missile Launcher
+        am_misl,
+        S_MISSILEUP,
+        S_MISSILEDOWN,
+        S_MISSILE,
+        S_MISSILE1,
+        S_MISSILEFLASH1
     },
     {
-	// bfg 9000
-	am_cell,
-	S_BFGUP,
-	S_BFGDOWN,
-	S_BFG,
-	S_BFG1,
-	S_BFGFLASH1
+        // Plasma Rifle
+        am_cell,
+        S_PLASMAUP,
+        S_PLASMADOWN,
+        S_PLASMA,
+        S_PLASMA1,
+        S_PLASMAFLASH1
     },
     {
-	// chainsaw
-	am_noammo,
-	S_SAWUP,
-	S_SAWDOWN,
-	S_SAW,
-	S_SAW1,
-	S_NULL
+        // BFG 9000
+        am_cell,
+        S_BFGUP,
+        S_BFGDOWN,
+        S_BFG,
+        S_BFG1,
+        S_BFGFLASH1
     },
     {
-	// super shotgun
-	am_shell,
-	S_DSGUNUP,
-	S_DSGUNDOWN,
-	S_DSGUN,
-	S_DSGUN1,
-	S_DSGUNFLASH1
-    },	
+        // Chainsaw
+        am_noammo,
+        S_SAWUP,
+        S_SAWDOWN,
+        S_SAW,
+        S_SAW1,
+        S_NULL
+    },
+    {
+        // Super Shotgun
+        am_shell,
+        S_DSGUNUP,
+        S_DSGUNDOWN,
+        S_DSGUN,
+        S_DSGUN1,
+        S_DSGUNFLASH1
+    },
 };
-
-
-
-
-
-
-
-
-
